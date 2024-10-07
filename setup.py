fa4202ac92ee854010a22fcf1fe7776f70d1a16b
Sunniva Josefsen <sunniva.josefsen@hotmail.com>
2024-10-07 19:58:26 +0200

    Initial commit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="morning_greetings",
+    name="Assignment_morning_greetings",
     version="0.1",
     packages=find_packages(),
     install_requires=[],
